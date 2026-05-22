f769321edfbcf1498ea632ced0fab18cbb5642aa
LL <you@example.com>
2026-05-22 11:22:35 +0800

    处理英汉大词典的词性跳转链接

--- deno/tool-1008.ts
+++ deno/tool-1008.ts
@@ -1,14 +1,9 @@
-/**
- * 处理牛津高阶英汉双解词典第10版完美版的oaldpe.txt文件，
- * 将每个单词条目中的英式发音部分放在美式发音部分之后，
- * 并将例句中英式发音放在美式发音之后。
- */
-// 使用deno读取文本文件全部内容
+
 import * as fs from 'node:fs';
 import * as readline from 'node:readline';
 
-const filePath = "D:\\codes\\tools\\tool-1001\\run.ts";
-const filePathNew = "D:\\codes\\tools\\run.ts.txt";
+const filePath = "C:\\Users\\L\\Documents\\MDDs\\英汉大词典（第2版）\\英汉大词典（第2版）_0.txt";
+const filePathNew = "C:\\Users\\L\\Documents\\MDDs\\英汉大词典（第2版）\\英汉大词典（第2版）.txt";
 // 文件内容过大，使用流式读取，一次读取一行
 const readStream = fs.createReadStream(filePath, { encoding: 'utf-8' });
 const writeStream = fs.createWriteStream(filePathNew, { encoding: 'utf-8' });
@@ -18,29 +13,39 @@ const rl = readline.createInterface({
     crlfDelay: Infinity // 自动识别所有CR/LF换行符
 });
 
+// 词头
+const hwRegex = /<span class="hw" homograph="(.*?)".*?>([^<]*).*?<\/span>/
+const hw2Regex = /<span class="hw".*?>([^<]*).*?<\/span>/
+// 词性
+const posgRegex = /(<div)( class="(?:se1|subse1)">(?:(?<!class="posg"|class="se1"|class="subse1").)*?<div class="posg">(.*?)<\/div>)/g
 
-const defTagRegex = /<span \s*class="phonetics"\s*>\s*(<div \s*class="phons_br"\s*>.*?<\/div>)\s*(<div \s*class="phons_n_am"\s*>.*?<\/div>)\s*<\/span>/gs;
-const defTagRegex2 = /<example-audio>\s*(<a [^<>]*class="audio_uk"[^<>]*?>.*?<\/a>)\s*(<a [^<>]*?class="audio_us"[^<>]*?>.*?<\/a>)\s*<\/example-audio>/gs;
-
-// 使用for await...of循环逐行迭代
-try {
-    let count = 0;
-    for await (const line of rl) {
-
-        count++;
-        console.log(line);
-
-        writeStream.write(`${line}\n`);
-
+for await (let line of rl) {
+    let anchorLinks = '<div class="top-anchor-links">'
+    let hw = ''
+    let hwMatch = line.match(hwRegex)
+    if (hwMatch) {
+        hw = hwMatch[2] + '_' + hwMatch[1]
+    } else {
+        hwMatch = line.match(hw2Regex)
+        if (hwMatch)
+            hw = hwMatch[1]
     }
-    console.log('>>> 文件处理完毕。count: ', count);
-} catch (err) {
-    console.error('>>> 处理文件时发生错误:', err);
+    if (hw) {
+        line = line.replaceAll(posgRegex, (m, p1, p2, p3, offset) => {
+            const id = hw + '_offset-' + offset
+            anchorLinks += `<a href="#${id}">${p3}</a>|`
+            return `${p1} id="${id}"${p2}`
+        })
+        if (anchorLinks.endsWith('|')) {
+            anchorLinks = anchorLinks.slice(0, -1)
+        }
+        anchorLinks += '</div>'
+        // line = anchorLinks + line
+        const target = /<link rel="stylesheet" type="text\/css" href="ecd.css" \/><div id="topAnchor"><\/div><a href="#topAnchor" style="position:fixed;right:0;bottom:0">回到顶部<\/a>.*?<div class="e" type="standard">/
+        line = line.replace(target, '<link rel="stylesheet" href="ecd.css" />' + anchorLinks + '<div class="e" type="standard">')
+    } else {       
+        // console.log('没有发现词头：', line)
+    }
+    writeStream.write(`${line}\n`);
 }
-
-function processContent(wordContent: string): string {
-    // 使用正则表达式替换
-    wordContent = wordContent.replace(defTagRegex, '<span class="phonetics">$2$1</span>');
-    wordContent = wordContent.replace(defTagRegex2, '<example-audio>$2$1</example-audio>');
-    return wordContent;
-}
+console.log('>>> 文件处理完毕。');
